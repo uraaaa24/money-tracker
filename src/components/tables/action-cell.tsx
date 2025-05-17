5be3a6162ed8f10378abd49213da916a7d40e35c
Yuto Urashima <yuto.urashima24@gmail.com>
2025-05-17 12:15:26 +0900

    feat: 削除ボタンにポップオーバーを追加し、確認メッセージを表示

--- src/components/tables/action-cell.tsx
+++ src/components/tables/action-cell.tsx
@@ -1,6 +1,9 @@
+import { PopoverClose } from '@radix-ui/react-popover'
 import { Pen, Trash } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 
 type EditButtonProps<T> = {
   item: T
@@ -26,19 +29,50 @@ type DeleteButtonProps = {
   onDelete: (itemId: number) => void
 }
 
-const DeleteButton = ({ itemId, onDelete }: DeleteButtonProps) => {
-  return (
-    <Button
-      variant="ghost"
-      size="icon"
-      className="cursor-pointer text-red-400 hover:text-red-500"
-      onClick={() => onDelete(itemId)}
+const DeleteButton = ({ itemId, onDelete }: DeleteButtonProps) => (
+  <Popover>
+    <PopoverTrigger asChild>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="cursor-pointer text-red-500 hover:text-red-600"
+      >
+        <span className="sr-only">Delete</span>
+        <Trash />
+      </Button>
+    </PopoverTrigger>
+
+    <PopoverContent
+      side="top"
+      align="center"
+      className="w-44 rounded-xl p-4 text-center space-y-4"
     >
-      <span className="sr-only">Delete</span>
-      <Trash />
-    </Button>
-  )
-}
+      <p className="text-sm font-medium">
+        Are you sure?
+      </p>
+
+      <div className="flex justify-center gap-2">
+        <PopoverClose asChild>
+          <Button variant="outline" size="sm"
+            className='cursor-pointer'
+          >
+            Cancel
+          </Button>
+        </PopoverClose>
+
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => onDelete(itemId)}
+          className="cursor-pointer text-red-500 border-red-200 hover:text-red-600 hover:bg-red-500/10"
+        >
+          Delete
+        </Button>
+      </div>
+    </PopoverContent>
+  </Popover>
+)
+
 
 type ActionCellProps<T extends { id: number }> = {
   item: T
